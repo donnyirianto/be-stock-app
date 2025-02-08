9d55aebe2d1d94f1577c716c20d5e2e367c4e2eb
zaranggi <donnyirianto.anggriawan@gmail.com>
2025-02-08 23:34:49 +0700

    update

--- modules/pengajuan/repository/add.go
+++ modules/pengajuan/repository/add.go
@@ -11,7 +11,7 @@ import (
 
 func (r *PengajuanRepositoryImpl) AddPengajuan(req *domain.RequestData, id, username string) error {
 
-	querySql := `INSERT INTO pengajuan (id,subject, keterangan, nama_user, status, created_at, updated_at) VALUES (?, ?, ?, 'PENDING', now(), now())`
+	querySql := `INSERT INTO pengajuan (id,subject, keterangan, nama_user, status, created_at, updated_at) VALUES (?, ?, ?, ?, 'PENDING', now(), now())`
 
 	result := r.mysqlConn.Exec(querySql, id, req.Subject, req.Keterangan, username)
 	if result.Error != nil {
